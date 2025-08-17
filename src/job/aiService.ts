09c460057eddc73548b6da2e61a29a3a1bfb0b7a
Avi Khandakar <avikhandakar@gmail.com>
2025-08-17 11:58:46 +0600

    update

--- src/job/aiService.ts
+++ src/job/aiService.ts
@@ -159,7 +159,7 @@ export async function sendPushNotificationsInBatches(
   tokens: string[]
 ) {
   const endpoint = "https://exp.host/--/api/v2/push/send";
-  const batchSize = 100;
+  const batchSize = 600;
   for (let i = 0; i < tokens.length; i += batchSize) {
     const batch = tokens.slice(i, i + batchSize);
 
